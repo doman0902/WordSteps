b71d3ed18e065c92e429e6a7514b034a7308d9ef
Fekete Domán Csaba <doman0902@gmail.com>
2025-10-09 17:48:02 +0200

    Created NavGraph.kt and MainMenuScreen.kt

--- AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -11,37 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph
 import com.example.androidapp.ui.theme.AndroidAppTheme
+import com.example.androidapp.navigation.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             AndroidAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavGraph()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidAppTheme {
-        Greeting("Android")
     }
 }
---
+++ AndroidApp/app/src/main/java/com/example/androidapp/navigation/NavGraph.kt
@@ -0,0 +1,4 @@
+package com.example.androidapp.navigation
+class NavGraph (){
+
+}
