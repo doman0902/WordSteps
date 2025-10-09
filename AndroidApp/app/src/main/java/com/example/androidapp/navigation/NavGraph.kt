1de57c1f2602624403daaed5be2253c5bf0ef66a
Fekete Domán Csaba <doman0902@gmail.com>
2025-10-09 18:30:50 +0200

    Modified NavGraph.kt and MainMenuScreen.kt

--- AndroidApp/app/src/main/java/com/example/androidapp/navigation/NavGraph.kt
+++ AndroidApp/app/src/main/java/com/example/androidapp/navigation/NavGraph.kt
@@ -1,4 +1,17 @@
 package com.example.androidapp.navigation
-class NavGraph (){
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.androidapp.ui.screens.*
+
+@Composable
+fun NavGraph(navController:NavHostController=rememberNavController()) {
+    NavHost(navController, startDestination = "main_menu"){
+        composable("main_menu") {MainMenuScreen(navController)}
+        //composable("quiz") { QuizScreen(navController) }
+    }
 
 }
--- AndroidApp/app/src/main/java/com/example/androidapp/ui/screens/MainMenuScreen.kt
+++ AndroidApp/app/src/main/java/com/example/androidapp/ui/screens/MainMenuScreen.kt
@@ -1,5 +1,51 @@
 package com.example.androidapp.ui.screens
 
-class MainMenuScreen (){
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainMenuScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("WordStep")
+                }
+            )
+        }
+    ) {padding ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+            .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center)
+        {
+            Button(onClick = {navController.navigate("quiz")},
+                modifier = Modifier.fillMaxWidth())
+            {
+                Text("Start Quiz")
+            }
+        }
+
+    }
+
+
 
 }
